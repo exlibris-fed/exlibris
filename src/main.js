ce613f2c0f7bcee264818960b4e127513758a894
Dan Conley <dconley@protonmail.com>
2020-03-08 22:46:11 -0400

    Add Register page
    It's not perfect, so I opened #47. In the course of this I discovered
    the solution for #36 as well.
    
    Fixes #36
    Closes #3

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import VueI18n from 'vue-i18n'
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import HomePage from './Views/Home.vue'
-import LoginPage from './Views/login.vue'
+import LoginPage from './Views/Login.vue'
+import Register from './Views/Register.vue'
 
 Vue.use(VueI18n)
 Vue.use(VueRouter)
@@ -13,7 +14,8 @@ Vue.config.productionTip = false
 
 const routes = [
   { path: '/', component: HomePage, name: 'home' },
-  { path: '/login', component: LoginPage, name: 'login' }
+  { path: '/login', component: LoginPage, name: 'login' },
+  { path: '/register', component: Register, name: 'register' }
 ]
 
 const router = new VueRouter({
