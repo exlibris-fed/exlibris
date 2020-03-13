b7ac7a9d7aca0cef4e7da7a6ff45a1a28259047e
Dan Conley <dconley@protonmail.com>
2020-03-12 21:29:02 -0400

    Add /logout (#51)
    As there's no session table on the back end this is a vue-only change.
    It also changes /login to skip the flow if an auth token is present.
    Dealing with auth tokens that aren't valid JWTs is handled in #34.
    
    Currently logout will redirect a user to /, which redirects to the login
    page. That should change, but as it stands the logout logic won't need
    to when it does.
    
    Closes #33

--- src/Views/Login.vue
+++ src/Views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="container">
-    <LoginForm />
+    <LoginForm :bounceto="bounceto" />
   </div>
 </template>
 
@@ -11,6 +11,19 @@ export default {
   name: 'LoginPage',
   components: {
     LoginForm
+  },
+  props: {
+    bounceto: {
+      type: String,
+      default: '/'
+    }
+  },
+  // this blindly assumes that having *an* auth token means a user is logged in.
+  // if the auth token isn't valid, it will log them out after the api returns 401.
+  created: function () {
+    if (localStorage.getItem('auth') !== null) {
+      this.$router.push(this.bounceto)
+    }
   }
 }
 </script>
---
+++ src/Views/Logout.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'LogoutPage',
+  props: {
+    bounceto: {
+      type: String,
+      default: '/'
+    }
+  },
+  created: function () {
+    localStorage.removeItem('auth')
+    this.$router.push(this.bounceto)
+  },
+  // we don't actually want to render anything. this is essentially a redirect.
+  render: function () {
+    return ''
+  }
+}
+</script>
