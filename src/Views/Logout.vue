26111b7309ee7a642ac80c17dc836e19ce2ae4df
Dan Conley <dconley@protonmail.com>
2020-07-09 10:06:20 -0400

    better header (#83)

--- src/Views/Logout.vue
+++ src/Views/Logout.vue
@@ -9,6 +9,7 @@ export default {
   },
   created: function () {
     localStorage.removeItem('auth')
+    this.$emit('logout')
     this.$router.push(this.bounceto)
   },
   // we don't actually want to render anything. this is essentially a redirect.
---
+++ src/components/HeaderProfile.vue
@@ -0,0 +1,57 @@
+<template>
+  <b-nav-item-dropdown
+    right
+  >
+    <template
+      v-slot:button-content
+    >
+      <b-img
+        width="50"
+        height="50"
+        alt=""
+        :src="icon"
+        rounded="circle"
+      />
+      {{ user.name }}
+    </template>
+
+    <b-dropdown-item
+      :to="profileLink"
+    >
+      {{ $t('profile') }}
+    </b-dropdown-item>
+    <b-dropdown-item
+      to="logout"
+    >
+      {{ $t('logout') }}
+    </b-dropdown-item>
+  </b-nav-item-dropdown>
+</template>
+
+<script>
+export default {
+  name: 'HeaderProfile',
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    icon () {
+      return (this.user && this.user.icon) || require('../../public/default-profile.jpg')
+    },
+    profileLink () {
+      return '@' + this.user.preferredUsername
+    }
+  },
+  i18n: {
+    messages: {
+      en: {
+        profile: 'Profile',
+        logout: 'Log Out'
+      }
+    }
+  }
+}
+</script>
