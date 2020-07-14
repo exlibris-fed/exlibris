08a68b560dcb78c25bc5fb9a194d1b7d81590b77
Matthew General <matthew.general@gmail.com>
2020-07-13 21:48:42 -0400

    Fix UserID type incorrectly set to string in outbox (#138)

--- model/outbox.go
+++ model/outbox.go
@@ -1,10 +1,12 @@
 package model
 
+import "github.com/google/uuid"
+
 // An OutboxEntry represents an entry in a user's AP outbox.
 type OutboxEntry struct {
 	Base
 	User       User
-	UserID     string
+	UserID     uuid.UUID
 	OutboxIRI  string
 	URI        string
 	Serialized string
